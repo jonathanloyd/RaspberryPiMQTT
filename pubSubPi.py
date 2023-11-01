eb1949768c4498eb87be6269f064f0268adf9a00
jloyd <jwloyd01@louisville.edu>
2023-11-01 11:45:02 -0400

    Add env vars and loop for subbed messages

--- pubSubPi.py
+++ pubSubPi.py
@@ -1,16 +1,23 @@
 import time
 import paho.mqtt.client as mqtt
+from dotenv import dotenv_values
+
+config = dotenv_values("pubSubPi.env")
+
+hostname = config["HOSTNAME"]
+broker_port = int(config["BROKER_PORT"])
+in_topic = config["IN_TOPIC"]
+out_topic = config["OUT_TOPIC"]
+
 
 def on_connect(client, userdata, flags, rc):
-	print("Connected to MQTT broker with result code: " + str(rc))
-	client.subscribe(topic)
+    print("Connected to MQTT broker with result code: " + str(rc))
+    client.subscribe(in_topic)
+
 
 def on_message(client, userdata, msg):
-	print("Message received: " + msg.payload.decode())
-
-hostname = "raspberrymqtthost"
-broker_port = 1883
-topic = "test/topic"
+    print("Message received: " + msg.payload.decode())
+    
 
 # Create MQTT client instance
 client = mqtt.Client()
@@ -23,5 +30,6 @@ client.on_message = on_message
 client.connect(hostname, broker_port, 60)
 
 payload = "test message from raspberrypi"
-print("Publishing " + payload + " to topic: " + topic)
-client.publish(topic, payload)
+print("Publishing " + payload + " to topic: " + out_topic)
+client.publish(out_topic, payload)
+client.loop_forever()
